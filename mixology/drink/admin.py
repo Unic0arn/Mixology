104ea1612321360d02fc5e0f5740ea304db96e2a
Fredrik Hallberg <fhallb@kth.se>
2014-03-07 04:51:36 +0100

    Created working models accessable via the admin interface

--- mixology/drink/admin.py
+++ mixology/drink/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from drink.models import Ingredient, Recipe, Drink, Tag
 # Register your models here.
+
+
+admin.site.register(Drink)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
+admin.site.register(Tag)
--- mixology/drink/models.py
+++ mixology/drink/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    unit = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+
+
+class Drink(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    upvotes = models.IntegerField()
+    downvotes = models.IntegerField()
+
+
+class Recipe(models.Model):
+
+    drinkID = models.ForeignKey('Drink')
+    ingredientID = models.ForeignKey('Ingredient')
+
+
+class Tag(models.Model):
+    drinkID = models.ForeignKey('Drink')
+    name = models.CharField(max_length=255)
