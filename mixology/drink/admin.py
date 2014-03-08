491d5a93fd66d92d627d9cf05372d9b5a59b6999
Fredrik Hallberg <fhallb@kth.se>
2014-03-08 06:40:56 +0100

    First page now shows drinks in database

--- mixology/drink/admin.py
+++ mixology/drink/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from drink.models import Ingredient, Recipe, Drink, Tag
+from drink.models import Ingredient, Drink, Tag
 # Register your models here.
 
 
 admin.site.register(Drink)
 admin.site.register(Ingredient)
-admin.site.register(Recipe)
 admin.site.register(Tag)
--- mixology/mixology/views.py
+++ mixology/mixology/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from drink.models import Drink
 
 
 def main(request):
-
-    return render(request, 'mixology/index.html')
+    drinks = Drink.objects.all()
+    return render(request, 'mixology/index.html', {'drinks': drinks})
